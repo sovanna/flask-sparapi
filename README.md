8394b2465cfb2cfcfa1bad388dca51a793b2eb9d
Sovanna <sovanna.hing@gmail.com>
2014-04-26 19:38:39 +0200

    start

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 flask-sparapi
 =============
 
+[![Build Status](https://magnum.travis-ci.com/sovanna/flask-sparapi.svg?token=qzFu9Qfosru1rcm6YHB5&branch=master)](https://magnum.travis-ci.com/sovanna/flask-sparapi)
+
 A fast and simple way to secure API for Flask applications
+
+Resources
+---------
+
+- [Documentation](http://packages.python.org/Flask-Sparapi) (coming soon)
+- [Issue Tracker](https://github.com/sovanna/flask-sparapi/issues)
+- [Source](https://github.com/sovanna/flask-sparapi)
+- [Development Version](https://github.com/sovanna/flask-sparapi/raw/develop#egg=Flask-Sparapi-dev)
---
+++ setup.py
@@ -0,0 +1,45 @@
+"""
+Flask-Sparapi
+==============
+
+Flask-Sparapi is a Flask extension that aims to add a fast and simple way
+to secure API for Flask applications.
+
+Resources
+---------
+
+- [Documentation](http://packages.python.org/Flask-Sparapi) (coming soon)
+- [Issue Tracker](https://github.com/sovanna/flask-sparapi/issues)
+- [Source](https://github.com/sovanna/flask-sparapi)
+- [Development Version](https://github.com/sovanna/flask-sparapi/raw/develop#egg=Flask-Sparapi-dev)
+
+"""
+
+from setuptools import setup
+
+setup(
+  name='Flask-Sparapi',
+  version='1.0.0',
+  url='https://github.com/sovanna/flask-sparapi',
+  license='BSD (3-Clause) License',
+  author='Sovanna Hing',
+  author_email='sovanna.hing@gmail.com',
+  description='Fast and simple way to secure API for Flask apps',
+  long_description=__doc__,
+  packages=['flask_sparapi'],
+  zip_safe=False,
+  include_package_data=True,
+  platforms='any',
+  install_requires=[
+    'Flask'
+  ],
+  classifiers=[
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+  ]
+)
